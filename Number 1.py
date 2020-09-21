1c70347c3e5c989a61983891107166d378b85d60
moniquesenjaya <70890949+moniquesenjaya@users.noreply.github.com>
2020-09-21 13:24:25 +0700

    Update Number 1.py

--- Number 1.py
+++ Number 1.py
@@ -1,4 +1,4 @@
-Degree = 150
-Radians = Degree * 3.14 / 180
+Degree = input("Input the number in degrees: ")
+Radians = float(Degree) * 3.14 / 180
 print("Degrees: " + str(Degree))
 print("Radians: " + str(Radians))
