1503a063fa0faa663c25b1691d4e18c4a16ad1e4
moniquesenjaya <70890949+moniquesenjaya@users.noreply.github.com>
2020-09-21 11:49:44 +0700

    Update Number 2.py

--- Number 2.py
+++ Number 2.py
@@ -1,1 +1,7 @@
-
+student1, student2, student3 = 80.0, 90.0, 66.5
+average = (student1 + student2 + student3)/3
+print("Students scores:")
+print(student1)
+print(student2)
+print(student3)
+print("Average score: " + str(average))
