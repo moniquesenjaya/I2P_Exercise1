100cacbff52ae77c8446d0ff7a0d3668c8816dae
moniquesenjaya <70890949+moniquesenjaya@users.noreply.github.com>
2020-09-21 11:56:36 +0700

    Update Number 3.py

--- Number 3.py
+++ Number 3.py
@@ -1,1 +1,9 @@
-
+class1, class2, class3 = 32, 45, 51
+print("Number of students in each group:")
+print("Class 1: " + str(class1//5))
+print("Class 2: " + str(class2//7))
+print("Class 3: " + str(class3//6))
+print("Number of students leftover:")
+print("Class 1: " + str(class1%5))
+print("Class 2: " + str(class2%7))
+print("Class 3: " + str(class3%6))
